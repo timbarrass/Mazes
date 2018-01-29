08d8112d787fa22ee4f660614a95b8919a10523d
timbarrass <tim.barrass@gmail.com>
2018-01-29 08:15:32 +0000

    First tests in scalatest
    Build-out of replacement maze interface

---
+++ com.timbarrass.mazes/test/Tests.scala
@@ -0,0 +1,170 @@
+import org.scalatest.{FlatSpec, MustMatchers}
+
+import scala.collection.mutable
+import scala.util.Random
+
+class MazeTest extends FlatSpec with MustMatchers {
+
+  behavior of "Maze"
+
+  "generateMaze" must "return a new Maze when requested" in {
+    generateMaze(3, 4) mustBe an[Maze]
+  }
+
+  it must "return a Maze with specified width and height" in {
+    val m = generateMaze(3, 4)
+    m.width must equal(3)
+    m.height must equal(4)
+  }
+
+  "maze" should "return a set of neighbour cells for a given cell" in {
+    val m = generateMaze(4, 4)
+    m(2, 3) mustBe an[mutable.Set[_]]
+  }
+
+  it must "have at least one route out of each cell" in {
+    val m = generateMaze(4, 4)
+    for (
+      y <- 0 until m.height;
+      x <- 0 until m.width
+    ) {
+      m(x, y).size must be > 0
+    }
+  }
+
+  it must "have a matching route in for each route out of a cell" in {
+    val m = generateMaze(4, 4)
+    for (
+      y <- 0 until m.height;
+      x <- 0 until m.width
+    ) {
+      val me = Cell(x, y)
+      m(x, y).foreach(n => {
+        m(n.x, n.y) must contain(me)
+      })
+    }
+  }
+
+  // now I can't tell that it's actually created a maze ...
+
+  "initNeighbours" must "return 4 neighbours for an internal cell" in {
+    val n = new Neighbours(6, 6)
+    n(3, 4) must contain(new Cell(3,5))
+    n(3, 4) must contain(new Cell(2,4))
+    n(3, 4) must contain(new Cell(4,4))
+    n(3, 4) must contain(new Cell(3,3))
+  }
+
+  it must "return 3 neighbours for a boundary cell" in {
+    val n = new Neighbours(6, 6)
+    n(0, 3) must contain(new Cell(0,2))
+    n(0, 3) must contain(new Cell(0,4))
+    n(0, 3) must contain(new Cell(1,3))
+    n(0, 3) must not contain(new Cell(-1,3))
+  }
+
+  def generateMaze(width:Int, height:Int): Maze = {
+    val neighbours = new Neighbours(width, height)
+
+    val c = randomCell(width, height)
+
+    val routes = process(
+      initGreyListNeighbours(c, neighbours),
+      initWhiteList(c, width, height),
+      emptyTree(width, height),
+      neighbours)
+
+    new Maze(routes)
+  }
+
+  private def process(grey: mutable.Set[Cell],
+                      white: Array[Array[Boolean]],
+                      tree: Array[Array[mutable.Set[Cell]]],
+                      neighbours: Neighbours): Array[Array[mutable.Set[Cell]]] =
+  {
+    if (grey.isEmpty) {
+      tree
+    } else {
+      Random.shuffle(grey) // ha so efficient
+      val c = grey.head // random?
+      grey -= c
+
+      white(c.x)(c.y) = true
+
+      // Prim's algo proper would choose single closest node. In this modified algo
+      // the closest nodes are the 4 cell neighbours, all 1 unit away -- so we
+      // just randomly choose one unconsidered neighbour
+      val n = neighbours(c.x, c.y).filter(n => { white(n.x)(n.y) }).head
+      tree(c.x)(c.y) += n
+      tree(n.x)(n.y) += c
+
+      process(grey union neighbours(c.x, c.y).filterNot( n => white(n.x)(n.y) ), white, tree, neighbours)
+    }
+  }
+
+  private def randomCell(width:Int, height:Int): Cell = {
+    Cell(r.nextInt(width - 1), r.nextInt(height - 1))
+  }
+
+  private def emptyTree(width: Int, height: Int): Array[Array[mutable.Set[Cell]]] = {
+    Array.fill[mutable.Set[Cell]](width, height) { mutable.Set() }
+  }
+
+  private def initWhiteList(c: Cell, width: Int, height: Int): Array[Array[Boolean]] = {
+    val white = Array.fill[Boolean](width, height) { false }
+
+    white(c.x)(c.y) = true
+
+    white
+  }
+
+  private def initGreyListNeighbours(c: Cell, neighbours: Neighbours): mutable.Set[Cell] = {
+    val grey = mutable.Set[Cell]()
+
+    for (n <- neighbours(c.x, c.y)) {
+      if (!grey.contains(n)) {
+        grey += n
+      }
+    }
+
+    grey
+  }
+
+
+
+
+
+
+  private val r = new Random
+
+  case class Cell(x:Int, y:Int) {  }
+
+  case class Neighbours(width:Int, height:Int) {
+    private val neighbours = Array.fill[mutable.Set[Cell]](width, height) {
+      mutable.Set()
+    }
+
+    for (
+      x <- 0 until width;
+      y <- 0 until height
+    ) {
+      if (x > 0) neighbours(x)(y) += Cell(x - 1, y)
+      if (x < width - 1) neighbours(x)(y) += Cell(x + 1, y)
+      if (y > 0) neighbours(x)(y) += Cell(x, y - 1)
+      if (y < height - 1) neighbours(x)(y) += Cell(x, y + 1)
+    }
+
+    def apply(x:Int, y:Int): mutable.Set[Cell] = {
+      neighbours(x)(y)
+    }
+  }
+
+  case class Maze(routes: Array[Array[mutable.Set[Cell]]]) {
+    val width: Int = routes.length
+    val height: Int  = routes(0).length
+
+    def apply(x: Int, y:Int): mutable.Set[Cell] = {
+      routes(x)(y)
+    }
+  }
+}
